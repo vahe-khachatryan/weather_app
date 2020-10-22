5fbacbdcb2e7ea2d5bfb4e56bbe2d6c06941c155
vahe-khachatryan <vahe.khachatryann@gmail.com>
2020-10-23 00:41:00 +0400

    Updates

---
+++ precache-manifest.ae106e747bfecc291d5545119f964bd5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941518971ea32d9ddca111a0a9f90f60",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "0695f18008e3c6b5f2ba",
+    "url": "/weather_app/static/css/2.f357938e.chunk.css"
+  },
+  {
+    "revision": "6d1a56c5cf1f27f97af9",
+    "url": "/weather_app/static/css/main.8b0dcb1f.chunk.css"
+  },
+  {
+    "revision": "0695f18008e3c6b5f2ba",
+    "url": "/weather_app/static/js/2.6be1dbcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_app/static/js/2.6be1dbcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1a56c5cf1f27f97af9",
+    "url": "/weather_app/static/js/main.c52342bb.chunk.js"
+  },
+  {
+    "revision": "4cc83ff11d5c1d5ff950",
+    "url": "/weather_app/static/js/runtime-main.c56cf221.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weather_app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weather_app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weather_app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weather_app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weather_app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "9922ccc2ab8167f43a9d9cf059300549",
+    "url": "/weather_app/static/media/weather.9922ccc2.jpg"
+  }
+]);
